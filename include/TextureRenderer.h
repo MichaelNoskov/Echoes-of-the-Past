b652d20d858ce6581a44bbbb512ed4f3d78f3dc3
MichaelNoskov <prosto.ya.chelovekn@mail.ru>
2025-11-17 12:00:01 +0300

    feat: теперь размеры вынесены на Spatial объекты, а Drawable просто рисует по принимаемым позиции и размерам

--- include/TextureRenderer.h
+++ include/TextureRenderer.h
@@ -7,36 +7,32 @@
 
 struct TextureDrawContext {
     Vector2 position;
-    float scale = 1;
+    Vector2 size;
     int method = 0;
 };
 
 class TextureRenderer : public Drawable<TextureDrawContext> {
 private:
     Texture2D texture;
-    float width;
-    float height;
     Vector2 offset;
 
-    void DrawBottom(Vector2 position, float scale);
-    void DrawCenter(Vector2 position, float scale);
-    void DrawTop(Vector2 position, float scale);
+    void DrawBottom(Vector2 position, Vector2 size);
+    void DrawCenter(Vector2 position, Vector2 size);
+    void DrawTop(Vector2 position, Vector2 size);
 
 public:
     TextureRenderer(
         const std::string& texturePath,
-        float width,
-        float height,
         Vector2 offset = {0, 0}
     );
 
     void Draw(const TextureDrawContext& context) override {
         if (context.method == 0) {
-            DrawBottom(context.position, context.scale);
+            DrawBottom(context.position, context.size);
         } else if (context.method == 1) {
-            DrawCenter(context.position, context.scale);
+            DrawCenter(context.position, context.size);
         } else if (context.method == 2) {
-            DrawTop(context.position, context.scale);
+            DrawTop(context.position, context.size);
         }
     }
 };
--- src/TextureRenderer.cpp
+++ src/TextureRenderer.cpp
@@ -1,12 +1,10 @@
-#include <TextureRenderer.h>
+#include "TextureRenderer.h"
 #include <raylib.h>
 
 TextureRenderer::TextureRenderer(
     const std::string& texturePath,
-    float width,
-    float height,
     Vector2 offset
-) : width(width), height(height), offset(offset) {
+) : offset(offset) {
     texture = LoadTexture(texturePath.c_str());
 }
 
@@ -14,26 +12,47 @@ TextureRenderer::~TextureRenderer() {
     UnloadTexture(texture);
 }
 
-void TextureRenderer::DrawBottom(Vector2 position, float scale) {
-    Vector2 drawPos = {
+void TextureRenderer::DrawBottom(Vector2 position, Vector2 size) {
+    Rectangle destRect = {
         position.x + offset.x,
-        position.y + offset.y - (height * scale)
+        position.y + offset.y - size.y,
+        size.x,
+        size.y
     };
-    DrawTextureEx(texture, drawPos, 0, scale, WHITE);
+    Rectangle sourceRect = {
+        0, 0, 
+        (float)texture.width, 
+        (float)texture.height
+    };
+    DrawTexturePro(texture, sourceRect, destRect, Vector2{0, 0}, 0, WHITE);
 }
 
-void TextureRenderer::DrawCenter(Vector2 position, float scale) {
-    Vector2 drawPos = {
-        position.x + offset.x - (width * scale) / 2,
-        position.y + offset.y - (height * scale) / 2
+void TextureRenderer::DrawCenter(Vector2 position, Vector2 size) {
+    Rectangle destRect = {
+        position.x + offset.x - size.x / 2,
+        position.y + offset.y - size.y / 2,
+        size.x,
+        size.y
     };
-    DrawTextureEx(texture, drawPos, 0, scale, WHITE);
+    Rectangle sourceRect = {
+        0, 0, 
+        (float)texture.width, 
+        (float)texture.height
+    };
+    DrawTexturePro(texture, sourceRect, destRect, Vector2{0, 0}, 0, WHITE);
 }
 
-void TextureRenderer::DrawTop(Vector2 position, float scale) {
-    Vector2 drawPos = {
+void TextureRenderer::DrawTop(Vector2 position, Vector2 size) {
+    Rectangle destRect = {
         position.x + offset.x,
-        position.y + offset.y
+        position.y + offset.y,
+        size.x,
+        size.y
     };
-    DrawTextureEx(texture, drawPos, 0, scale, WHITE);
+    Rectangle sourceRect = {
+        0, 0, 
+        (float)texture.width, 
+        (float)texture.height
+    };
+    DrawTexturePro(texture, sourceRect, destRect, Vector2{0, 0}, 0, WHITE);
 }
